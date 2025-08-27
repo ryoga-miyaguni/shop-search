f151cbbf9ef41e03fabf13042cdf8b77dcf4138c
Shuto TUHA <e235710@ie.u-ryukyu.ac.jp>
2025-08-27 15:59:21 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,21 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
+       width={180}
           height={38}
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
+       <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+         src/app/page.tsx
+</code>
             .
           </li>
-          <li className="tracking-[-.01em]">
+  <li className="tracking-[-.01em]">
             Save and see your changes instantly.
-          </li>
+    </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
